e26561f95493e3683ee2dbdfa4e8600eacef6bb4
Matt Carr <mpcarr84@gmail.com>
2017-04-14 22:26:51 +0100

    Update barnacle.py

--- barnacle.py
+++ barnacle.py
@@ -3,7 +3,7 @@ import logging
 from socketIO_client import SocketIO, LoggingNamespace
 
 FORMAT = '%(asctime)-15s %(message)s'
-logging.basicConfig(format=FORMAT,filename=barnacle.log)
+logging.basicConfig(format=FORMAT,filename='barnacle.log')
 logger = logging.getLogger()
 logger.info('started barnacle')
 
