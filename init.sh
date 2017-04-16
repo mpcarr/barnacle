4d6110fb12d152702e770041d86b75907a2b40c0
Matt Carr <mpcarr84@gmail.com>
2017-04-16 14:47:00 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
 echo "init script"
+apt-get install python-dev python-rpi.gpio
+sudo chown root.gpio /dev/gpiomem
+sudo chmod g+rw /dev/gpiomem
+
+
+
 sudo cp barnacle.service /lib/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable barnacle.service
