e170a7d139cc5b27a9c58f70898c684720876f17
Matt Carr <mpcarr84@gmail.com>
2017-04-16 14:06:45 +0100

    Update barnacle.py

--- barnacle.py
+++ barnacle.py
@@ -1,5 +1,6 @@
 import signal
 import logging
+import knob
 from socketIO_client import SocketIO, LoggingNamespace
 
 FORMAT = '%(asctime)-15s %(message)s'
@@ -7,5 +8,48 @@ logging.basicConfig(format=FORMAT,filename='/home/volumio/barnacle/barnacle.log'
 logger = logging.getLogger()
 logger.info('started barnacle')
 
-print("barnacle")
-signal.pause()
+# The two pins that the encoder uses (BCM numbering).
+GPIO_A = 26   
+GPIO_B = 19
+
+# The pin that the knob's button is hooked up to. If you have no button, set
+# this to None.
+GPIO_BUTTON = 13
+
+# This callback runs in the background thread. All it does is put turn
+# events into a queue and flag the main thread to process them. The
+# queueing ensures that we won't miss anything if the knob is turned
+# extremely quickly.
+def on_turn(delta):
+  print("%f", delta);
+  #QUEUE.put(delta)
+  #EVENT.set()
+  
+def on_press(value):
+  print("Toggled button")
+  #EVENT.set()  
+
+def setup():
+	GPIO.setmode(GPIO.BCM)       # Numbers GPIOs by physical location
+  global encoder = RotaryEncoder(GPIO_A, GPIO_B, callback=on_turn, buttonPin=GPIO_BUTTON, buttonCallback=on_press)
+
+#def loop():
+	#global globalCounter
+	#while True:
+		#rotaryDeal()
+    #print 'globalCounter = %d' % globalCounter
+
+def destroy():
+	GPIO.cleanup()             # Release resource
+
+if __name__ == '__main__':     # Program start from here
+	setup()
+	try:
+    print("barnacle")
+    signal.pause()
+		#loop()
+	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		destroy()
+
+
+
