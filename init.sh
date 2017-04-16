969bc3ddd5b16bbb01491864d5833c82380fdab0
Matt Carr <mpcarr84@gmail.com>
2017-04-16 16:28:37 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 echo "init script"
 pip install -r barnacle/requirements.txt
-apt-get install python-smbus
+#apt-get install python-smbus
 #apt-get install python-dev python-rpi.gpio
 sudo chown root.gpio /dev/gpiomem
 sudo chmod g+rw /dev/gpiomem
