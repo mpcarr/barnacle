06ee30ea8127b5e35c88324cf9abe4bf592ebd7f
Matt Carr <mpcarr84@gmail.com>
2017-04-17 21:39:59 +0100

    Update volumio.py

--- volumio.py
+++ volumio.py
@@ -4,37 +4,37 @@ class VolumioAPI:
   
   #global logger
   
-  def __init__(self, log):
+  def __init__(self, log, lcd):
     self.logger = log
-    self.logger.info('volumio socket init: connecting...') 
+    self.lcd = lcd
+    self.logger.info('volumio socket init: connecting...')
+    self.lcd_display_string("connecting to Volumio", 2)
     self.socketIO = SocketIO('localhost', 3000)
     self.socketIO.on('connect', self.on_connect)
     self.socketIO.on('disconnect', self.on_disconnect)
     self.socketIO.on('reconnect', self.on_reconnect)
-    self.socketIO.on('pushState', self.on_pushState)
+    self.socketIO.on('pushBrowseSources', self.on_browseSources)    
     self.socketIO.wait(seconds=1)
-    self.testLogger()
-
-  def testLogger(self):
-    self.logger.info('test logger')
+   
+  def menuDown:
+    self.logger.info('menu down')
+    
+  def menuUp:
+    self.logger.info('menu up')
+    
+  def enter:
+    self.logger.info('enter')
     
   def on_connect(self):
-    print('connect')
-    self.socketIO.on('pushBrowseSources', self.on_browseSources)
+    self.lcd_display_string("CONNECTED", 2)
     self.socketIO.emit('getBrowseSources', {})
     self.socketIO.wait(seconds=1)
 
   def on_browseSources(self, *args):
-    print(args)
+    self.logger.info(args)
     
   def on_disconnect(self):
     print('disconnect')
 
   def on_reconnect(self):
     print('reconnect')
-  
-  def on_pushState(self, *args):
-    print(args)     
-    
-  def getBrowseableSources(self):
-    self.logger.info('get sources')
