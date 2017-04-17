e64cf620f2148d3727438f287953b2b131fc1104
Matt Carr <mpcarr84@gmail.com>
2017-04-17 20:17:11 +0100

    Update volumio.py

--- volumio.py
+++ volumio.py
@@ -2,22 +2,28 @@ from socketIO_client import SocketIO, LoggingNamespace
 
 class VolumioAPI:
   
-  def __init__(self, logger):
-    self.logger = logger
-    self.logger.info('volumio socket init: connecting...') 
+  global logger
+  
+  def __init__(self, log):
+    logger = log
+    logger.info('volumio socket init: connecting...') 
     socketIO = SocketIO('localhost', 3000)
     socketIO.on('connect', self.on_connect)
     socketIO.on('disconnect', self.on_disconnect)
     socketIO.on('reconnect', self.on_reconnect)
+    testLogger()
+    
+  def testLogger()
+    logger.info('test logger')
     
   def on_connect(self):
-    self.logger.info('connect')
+    logger.info('connect')
 
   def on_disconnect(self):
-    self.logger('disconnect')
+    logger('disconnect')
 
   def on_reconnect(self):
-    self.logger('reconnect')    
+    logger('reconnect')    
     
   def getBrowseableSources(self):
-    self.logger.info('get sources')
+    logger.info('get sources')
