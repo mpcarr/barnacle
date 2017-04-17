a9331ffca11b86a00574d9e997b64740bad7d4c3
Matt Carr <mpcarr84@gmail.com>
2017-04-17 22:22:29 +0100

    Update volumio.py

--- volumio.py
+++ volumio.py
@@ -26,7 +26,7 @@ class VolumioAPI:
         self.lcd.lcd_clear()
         self.lcd.lcd_display_string("Volumio connecting..", 2)
         self.lcd.lcd_display_string("Timeout in  {}".format(connection_timeout), 3)
-        self.socketIO.wait(seconds=1)
+        #self.socketIO.wait(seconds=1)
         sleep(1)
         connection_timeout = connection_timeout - 1
       
