85eddd955cf0d72cb86f8e4b44bba05c46b856be
Matt Carr <mpcarr84@gmail.com>
2017-04-17 20:05:03 +0100

    Update volumio.py

--- volumio.py
+++ volumio.py
@@ -1,1 +1,23 @@
+from socketIO_client import SocketIO, LoggingNamespace
 
+class VolumioAPI:
+  
+  def __init__(self, logger):
+    self.logger = logger
+    self.logger.info('started barnacle') 
+    socketIO = SocketIO('localhost', 3000)
+    socketIO.on('connect', on_connect)
+    socketIO.on('disconnect', on_disconnect)
+    socketIO.on('reconnect', on_reconnect)
+    
+  def on_connect(self):
+    self.logger.info('connect')
+
+  def on_disconnect(self):
+    self.logger('disconnect')
+
+  def on_reconnect(self):
+    self.logger('reconnect')    
+    
+  def getBrowseableSources(self):
+    self.logger.info('get sources')
