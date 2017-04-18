3941bf2866a3e31952c92a8a7571e96dffdd87b6
Matt Carr <mpcarr84@gmail.com>
2017-04-18 20:52:43 +0100

    Update barnacle.py

--- barnacle.py
+++ barnacle.py
@@ -12,6 +12,11 @@ GPIO_B = 17
 # The pin that the knob's button is hooked up to.
 GPIO_BUTTON = 23
 
+FORMAT = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=FORMAT,filename='/home/volumio/barnacle/barnacle.log',level=logging.INFO)
+logger = logging.getLogger()
+logger.info('started barnacle')
+
 def on_turn(delta):
   if delta == -1:
     volumioCtrl.menuDown()
@@ -22,11 +27,7 @@ def on_press(value):
   volumioCtrl.enter()
 
 def setup():
-  FORMAT = '%(asctime)-15s %(message)s'
-  logging.basicConfig(format=FORMAT,filename='/home/volumio/barnacle/barnacle.log',level=logging.INFO)
-  logger = logging.getLogger()
-  logger.info('started barnacle')	
-	
+  logger.info('in setup')
   GPIO.setmode(GPIO.BCM)       # Numbers GPIOs by physical location
   global encoder
   encoder  = knob.RotaryEncoder(GPIO_A, GPIO_B, callback=on_turn, buttonPin=GPIO_BUTTON, buttonCallback=on_press)
