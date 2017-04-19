3e93473ce3fab7922f7b3fcef9da3a96dfad53b7
Matt Carr <mpcarr84@gmail.com>
2017-04-19 20:20:55 +0100

    Update volumio_controller.py

--- volumio_controller.py
+++ volumio_controller.py
@@ -3,6 +3,19 @@ from time import sleep
 import Queue
 import threading
 
+
+def connect_to_socket(q):
+  print("connect")
+  try:
+    #socket = SocketIO('localhost', 3000)
+    #socket.on('connect', self.on_connect)
+    #socket.on('disconnect', self.on_disconnect)
+    #socket.on('reconnect', self.on_reconnect)
+    #socket.wait(seconds=1)
+    q.put(1)
+  except e:
+    #self.logger.info(e)
+
 class VolumioApi:
   
   #global logger  
@@ -68,15 +81,3 @@ class VolumioApi:
   def on_reconnect(self):
     print('reconnect')
     
-    
-def connect_to_socket(q):
-  print("connect")
-  try:
-    #socket = SocketIO('localhost', 3000)
-    #socket.on('connect', self.on_connect)
-    #socket.on('disconnect', self.on_disconnect)
-    #socket.on('reconnect', self.on_reconnect)
-    #socket.wait(seconds=1)
-    q.put(1)
-  except e:
-    #self.logger.info(e)
