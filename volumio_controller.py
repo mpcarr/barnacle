d6114fdd81d37371553aacd06ea025f7811b75b3
Matt Carr <mpcarr84@gmail.com>
2017-04-20 09:11:46 +0100

    Update volumio_controller.py

--- volumio_controller.py
+++ volumio_controller.py
@@ -110,6 +110,7 @@ class VolumioApi:
     self.logger.info(data)
     line = 1
     for musicSource in data:
+      self.logger.info("loop")	
       #if line < 5:	
         #self.lcd.lcd_write(musicSource[0]["name"],line,1)
         #line = line + 1
