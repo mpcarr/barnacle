26f5810aaa8e9a2868bd3d9030310e6c2dbab3e8
Matt Carr <mpcarr84@gmail.com>
2017-04-20 17:38:40 +0100

    Update volumio_controller.py

--- volumio_controller.py
+++ volumio_controller.py
@@ -90,8 +90,8 @@ class VolumioApi:
     #clear the selection arrow
     self.lcd.lcd_write(0x02) # return home
     self.lcd.lcd_write(0x80) # line 1 pos 1
-    self.lcd.lcd_display_string("A")
-	
+    #self.lcd.lcd_display_string("A")
+    self.lcd.lcd_write_char(0)	
 	
     #self.lcd.lcd_write(self.lines[self.currentLine])
     #self.lcd.lcd_write_char(0)   
